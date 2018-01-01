f58f1da752b1833c853498e8293d082171bdc75c
VikVelev <viktorvelev8@gmail.com>
2018-01-01 19:49:39 +0200

    Movie script should be working, Training is needed, if someone has a cool cpu and a lot of time, feel free to train it

--- Movies/main.py
+++ Movies/main.py
@@ -1,80 +1,69 @@
 import os
-import re
 import pickle
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.neighbors import NearestNeighbors
 
+
 def here(file):
     return os.path.dirname(os.path.realpath(__file__)) + file
 
+
 if os.path.exists(here("/data.sav")) and os.path.exists(here("/distances.sav")) and os.path.exists(here("/indices.sav")):
-    movieData = pickle.load(open(here('/data.sav'),'rb'))
-    distances = pickle.load(open(here('/distances.sav'),'rb'))
-    indices = pickle.load(open(here('/indices.sav'),'rb'))
+    movieData = pickle.load(open(here('/data.sav'), 'rb'))
+    distances = pickle.load(open(here('/distances.sav'), 'rb'))
+    indices = pickle.load(open(here('/indices.sav'), 'rb'))
 
 else:
-    print("Loading data...")    
+    print("Loading data...")
 
-    movieData = pd.read_table(here("/data/basics"),low_memory=False)
-    movieRatings = pd.read_table(here("/data/ratings"))
-    movieData = np.array_split(movieData, 2)[0]
-    movieData = movieData.drop(["endYear","isAdult","runtimeMinutes"],axis=1)
+    movieData = pd.read_table(here("/data/basics"), low_memory=False)
+    movieData = movieData.dropna()
+    movieData = movieData.drop(["endYear", "isAdult"], axis=1)
 
-    #the parameters I consider the most important
-
-    f_Genre = movieData.genres.str
-    print("Converted to string.")
+    # the parameters I consider the most important
     print("Converting to dummies.")
-    f_Genre = f_Genre.get_dummies(sep=",")
+    f_Genre = movieData.genres.str.get_dummies(sep=",")
+    movieData[movieData["startYear"] == "\\N"] = "0"
+    f_StartYear = movieData.startYear
     f_Type = pd.get_dummies(movieData.titleType)
-    f_Rating = movieRatings.averageRating
-    print(f_Genre)
-    print("=========================================")
-    print(f_Type)
-    print("=========================================")
-    print(f_Rating)
-    
-    #Assemble the training data
-    movieFeatures = pd.concat([f_Genre, f_Type, f_Rating],axis=1)
-    movieFeatures = movieFeatures.dropna()
+
+    # Assemble the training data
+    movieFeatures = pd.concat([f_Genre, f_Type, f_StartYear], axis=1)
     print("Training...")
-    #MaxAbsScaler is used with sparce matrices for capping their values between 1 and 0
+    print("Number of movies %s." % len(movieFeatures))
     movieFeatures = MaxAbsScaler().fit_transform(movieFeatures)
 
-    #using KNN to get similar anime.
-    #number of neighbors represents how many similar animes you will get
+    # number of neighbors represents how many similar shows you will get
     nbrs = NearestNeighbors(n_neighbors=6, algorithm='ball_tree').fit(movieFeatures)
     distances, indices = nbrs.kneighbors(movieFeatures)
 
     with open(here('/data.sav'), 'wb') as file:
         pickle.dump(movieData, file, pickle.HIGHEST_PROTOCOL)
-    
+
     with open(here('/distances.sav'), 'wb') as file:
         pickle.dump(distances, file, pickle.HIGHEST_PROTOCOL)
-    
+
     with open(here('/indices.sav'), 'wb') as file:
         pickle.dump(indices, file, pickle.HIGHEST_PROTOCOL)
 
+allMovies = list(movieData.originalTitle)
 print("Loaded.")
 
-def getIndex(name):
-    if movieData[movieData["originaname"]==name].index.tolist() == []:
-        if not getIdPartialName(name) == []:
 
+def getIndex(name):
+    if movieData[movieData["originalTitle"] == name].index.tolist() == []:
+        if not getIdPartialName(name) == []:
             print("There are multiple choices")
             print("------------------------------")
-            
+
             iterator = 1
             for label in getIdPartialName(name):
-                print(("%s. %s") % (iterator,label))
+                print(("%s. %s") % (iterator, label))
                 iterator += 1
 
-            print("------------------------------")            
+            print("------------------------------")
             print("Which one should I search for? (number only)")
             choice = int(input()) - 1
             print("------------------------------")
@@ -82,11 +71,10 @@ def getIndex(name):
         else:
             print("Nothing found.")
     else:
-        return movieData[movieData["name"]==name].index.tolist()[0]
+        return movieData[movieData["originalTitle"] == name].index.tolist()[0]
 
-allMovies = list(movieData.originalTitle)
 
-#because most names are 5+ words this makes it so it will be enough to type just one word.
+# basically autocomplete
 def getIdPartialName(partial):
     movieArray = []
     for name in allMovies:
@@ -94,26 +82,25 @@ def getIdPartialName(partial):
             movieArray.append(name)
     return movieArray
 
-def printMovies(query=None,id=None):
-    print("------------------------------")    
+
+def printMovies(query=None, id=None):
+    print("------------------------------")
     print("Here are some recommendations:")
     print("------------------------------")
-    
+
     if id:
         for id in indices[id][1:]:
-            print(animeData.ix[id]["name"])
+            print(movieData.iloc[id]["originalTitle"])
     if query:
         found_id = getIndex(query)
         for id in indices[found_id][1:]:
-            print("______________________________")            
-            print(movieData.ix[id]["name"])
-            print(movieData.ix[id]["genre"])
-            print(("Rating: %s") % movieData.ix[id]["rating"])
-            print()
+            print("______________________________")
+            print(movieData.iloc[id]["originalTitle"])
+            print(movieData.iloc[id]["genres"])
 
 
-#[print("------------------------------")       
-#print("Tell me a movie/tv show:")
-print(movieData)
-# inputMovie = input()
-# printMovies(query=inputMovie)
+print("Tell me a movie/tv show:")
+inputMovie = input()
+print(len(indices))
+print(len(allMovies))
+printMovies(query=inputMovie)
